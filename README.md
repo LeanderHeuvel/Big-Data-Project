ff21f0ebcdd365b186d8de027078896292d090e8
l.p.w.vandenheuvel@student.utwente.nl <s2847868@ctit011.ewi.utwente.nl>
2023-01-16 14:38:02 +0100

    added absolute path for multiple user use

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Big-Data-Project
 This is the repository of the big data project, 2023.
 
+
+
--- project_feature_dct.py
+++ project_feature_dct.py
@@ -1,6 +1,6 @@
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
-
+import os
 from pyspark.ml.feature import VectorAssembler, DCT
 
 from pyspark.ml.linalg import Vectors
@@ -9,7 +9,12 @@ spark = SparkContext(appName='feature_extraction')
 
 sc = SparkSession(spark)
 
-C1 = spark.wholeTextFiles('Sensor3')
+base_path = "hdfs://ctit048.ewi.utwente.nl/user/s2640996"
+
+sensor_name = "Sensor3"
+
+C1 = spark.wholeTextFiles(os.path.join(base_path,sensor_name))
+
 
 df1 = C1.map(lambda (path, data) : (path.split('row')[1], Vectors.dense(data.split(',')))).toDF()
 
