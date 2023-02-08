14ac9e08fa48c5ad19d8a510fc9c0f953b51c867
l.p.w.vandenheuvel@student.utwente.nl <s2847868@ctit011.ewi.utwente.nl>
2023-02-08 14:28:57 +0100

    fixed covariance dimensionality and reverted project_file_test.py

--- project_file_efficient.py
+++ project_file_efficient.py
@@ -7,6 +7,7 @@ from pyspark.ml.linalg import Vectors
 import math
 import numpy as np
 import os
+from os import path
 
 base_path = "hdfs://ctit048.ewi.utwente.nl/user/s2640996"
 
@@ -21,12 +22,19 @@ spark = SparkSession.builder.appName('project').getOrCreate()
 sensor3 = sc.wholeTextFiles(os.path.join(base_path,"Sensor3"))
 sensor3 = sensor3.map(lambda (path, data) : (path.split('row')[1], data.split(','))).toDF()
 sensor3 = sensor3.select(col('_1'), col("_2").alias("sensor3_measurement").cast(ArrayType(DoubleType())))
+sensor3 = sensor3.limit(10)
+
 
 sensor4 = sc.wholeTextFiles(os.path.join(base_path,"Sensor4"))
 sensor4 = sensor4.map(lambda (path, data) : (path.split('row')[1], data.split(','))).toDF()
 sensor4 = sensor4.select(col('_1'), col("_2").alias("sensor4_measurement").cast(ArrayType(DoubleType())))
+sensor4 = sensor4.limit(10)
 
 sensors = sensor4.join(sensor3, on=['_1']).rdd
-cov = sensors.map(lambda x: (x[0], np.cov(x[1], x[2]).tolist()))
-acos = cov.map(lambda x: math.acos(x[0],x[1]))
-print(acos.count())
+# print(len(sensors.take(1)[0][1]))
+cov = sensors.map(lambda x: (x[0], np.cov(x[1], x[2])[0][1]))
+print("cov first take",cov.take(1))
+
+# acos = cov.map(lambda x: math.acos(x[1]))
+
+# acos.take(1)
--- project_file_test.py
+++ project_file_test.py
@@ -4,6 +4,9 @@ from pyspark.sql.functions import col,explode, monotonically_increasing_id
 from pyspark.sql.types import IntegerType, DoubleType, StructField, StructType, ArrayType
 from pyspark.ml.linalg import Vectors
 
+import os
+import math
+
 base_path = "hdfs://ctit048.ewi.utwente.nl/user/s2640996"
 
 sensor_name = "Sensor3"
@@ -15,24 +18,25 @@ sc = SparkContext(appName='feature_extraction')
 spark = SparkSession.builder.appName('project').getOrCreate()
 
 
-empty_rdd =spark.emptyRDD()
+empty_rdd = sc.emptyRDD()
 columns = StructType([StructField('ID', IntegerType(), False),
 		      StructField('PhaseDelta', DoubleType(), False)])
 
-feature_df = sc.createDataFrame(data=empty_rdd, schema=columns)
+feature_df = spark.createDataFrame(data=empty_rdd, schema=columns)
 feature_df.show()
 counter = 0
 
 
 ### READ LABEL
-labels = sc.read.csv(os.path.join(base_path, labels_name), header=True)
+labels = spark.read.csv(os.path.join(base_path, labels_name), header=True).collect()
 
 
 for measurement in labels:
-     if counter > 100:
+     if counter > 1:
           break
-     c1 = sc.read.text(os.path.join(base_path,'Sensor3/row')+str(measurement['Index'])+'.csv', lineSep=',').toDF('C1')
-     c2 = sc.read.text(os.path.join(base_path,'Sensor4/row')+str(measurement['Index'])+'.csv', lineSep=',').toDF('C2')
+
+     c1 = spark.read.text(os.path.join(base_path,'Sensor3/row')+str(measurement['Index'])+'.csv', lineSep=',').toDF('C1')
+     c2 = spark.read.text(os.path.join(base_path,'Sensor4/row')+str(measurement['Index'])+'.csv', lineSep=',').toDF('C2')
      
      c1 = c1.withColumn('C1', c1.C1.cast('double'))
      c1 = c1.select('*').withColumn('id', monotonically_increasing_id())
@@ -41,10 +45,11 @@ for measurement in labels:
 
      c2 = c2.select('*').withColumn('id', monotonically_increasing_id())
 
-     # c1c2 = c1.join(c2, on=['id'])
-     feature = get_phase_shift(c1['C1'],c2['C2'])
-     # feature = math.acos(c1c2.cov('C1', 'C2'))
-     temp_df = sc.createDataFrame([(measurement['Index'], feature)], ['ID', 'PhaseDelta'])
+     c1c2 = c1.join(c2, on=['id'])
+     print("c1c2: ",c1c2.count()," ",len(c1c2.columns))
+     print("covariance: ",c1c2.cov('C1', 'C2'))
+     feature = math.acos(c1c2.cov('C1', 'C2'))
+     temp_df = spark.createDataFrame([(measurement['Index'], feature)], ['ID', 'PhaseDelta'])
      feature_df = feature_df.union(temp_df)
      counter = counter + 1
 
