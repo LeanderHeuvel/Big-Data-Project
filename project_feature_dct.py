e7f976e3d3827b7d4422585a72f87ce8eb365739
r.zubavicius@student.utwente.nl <s2640996@ctit071.ewi.utwente.nl>
2023-02-10 10:50:17 +0100

    magnituded of C1 and C2 via fft approach

--- project_feature_dct.py
+++ project_feature_dct.py
@@ -4,7 +4,7 @@ import os
 from pyspark.sql.functions import col
 from pyspark.sql.functions import udf
 from pyspark.ml.feature import VectorAssembler, DCT
-
+from pyspark.sql.types import FloatType
 from pyspark.ml.linalg import Vectors, VectorUDT
 # https://stackoverflow.com/questions/32788387/pipelinedrdd-object-has-no-attribute-todf-in-pyspark
 spark = SparkContext(appName='feature_extraction')
@@ -33,13 +33,13 @@ df_dct2 = dct.transform(df2)
 def max_val(vector):
 	return  max(vector)
 
-max_udf = udf(max_val)
+max_udf = udf(max_val, FloatType())
 
-df_dct1 = df_dct1.withColumn('valuesC1', max_udf(df_dct1['result']))
-df_dct2 = df_dct2.withColumn('valuesC2', max_udf(df_dct2['result']))
+df_dct1 = df_dct1.withColumn('valuesC1', max_udf(df_dct1['result'])).select(col('_1').alias('C1I'), col('valuesC1'))
+df_dct2 = df_dct2.withColumn('valuesC2', max_udf(df_dct2['result'])).select(col('_1').alias('C2I'), col('valuesC2'))
 
-combined = df_dct1.join(df_dct2, df_dct1._1 == df_dct2._1)
-combined.select(col('_1'), col('valuesC1'), col('valuesC2')).write.csv('magn_test2')
+combined = df_dct1.join(df_dct2, df_dct1.C1I == df_dct2.C2I)
+combined.select(col('C1I'), col('valuesC1'), col('valuesC2')).write.csv('magn_test')
 # print(df2.select('result').head(1))
 
 
