f36cad20144268c3fe659fc5da919aafa2a2f9ee
Leander van den Heuvel <l.p.w.vandenheuvel@student.utwente.nl>
2023-02-12 11:16:18 +0100

    Added code for ridge regression

--- decision_tree_regression.py
+++ decision_tree_regression.py
@@ -116,5 +116,6 @@ print("Mean absolute error: ", evaluator.evaluate(model.transform(test), {evalua
 print("Root mean squared error: ", evaluator.evaluate(model.transform(test), {evaluator.metricName: "rmse"}))
 print("Mean squared error: ", evaluator.evaluate(model.transform(test), {evaluator.metricName: "mse"}))
 print("R2 score: ", evaluator.evaluate(model.transform(test), {evaluator.metricName: "r2"}))
-prediction_results = "Gaussian_linear_predictions"
+prediction_results = "Ridge_Regression"
 model.transform(test).write.parquet(prediction_results)
+
